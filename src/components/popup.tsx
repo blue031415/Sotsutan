f87474fbf0c7b2ed9e912731aca9b650d544b990
blue031415 <blue031415@gmail.com>
2024-11-13 14:03:19 +0900

    成績ファイル説明のモーダルの実装
    Co-authored-by: B.M. <mikadonec@users.noreply.github.com>

--- src/components/popup.tsx
+++ src/components/popup.tsx
@@ -1,32 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import styles from "./../styles/popup.module.css";
 
 const PopUp = () => {
   const [isPopUpVisible, setPopUpVisible] = useState(false);
+  const popUpRef = useRef(null);
+
   const togglePopUp = () => {
     setPopUpVisible(!isPopUpVisible);
   };
 
+  useEffect(() => {
+    if (isPopUpVisible && popUpRef.current) {
+      popUpRef.current.focus();
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isPopUpVisible]);
+
   return (
     <>
       <div>
-        <button onClick={togglePopUp}>ポップアップを開く</button>
+        <button onClick={togglePopUp}>CSVファイルの出力方法はこちら</button>
         {isPopUpVisible && (
           <div className={styles.PopUp}>
-            <div className={styles.Explain}>
+            <div
+              className={styles.Explain}
+              id="PopUp" // IDを一致させる
+              tabIndex={-1} // フォーカス可能にする
+              ref={popUpRef} // useRefを使用
+              role="dialog" // アクセシビリティ向上
+              aria-modal="true" // アクセシビリティ向上
+            >
+              <h3>twinsから成績データをダウンロードする方法</h3>
               <p>1. まずtwinsにログインします。</p>
-              <img src="/login.png"></img>
+              <img src="/login.png" alt="ログイン画面" />
               <p>2. 「成績」を選択します。</p>
-              <img src="/Select_grade.png"></img>
+              <img src="/select_grade.png" alt="成績選択画面" />
               <p>3. 「成績」にアクセスできたら一番下までスクロールします。</p>
               <p>4. 「ダウンロード」ボタンを押します。</p>
-              <img src="/csvExport3.PNG"></img>
+              <img src="/csvExport3.PNG" alt="ダウンロードボタン" />
               <p>
                 5. ファイル形式は「csv」、文字コードは「Unicode
-                (UTF-8)を選択します。
+                (UTF-8)」を選択します。
               </p>
               <p>6. 「出力」ボタンを押します。これでダウンロード完了です！</p>
-              <img src="/grade_output.png"></img>
+              <img src="/grade_output.png" alt="出力完了画面" />
             </div>
             <button onClick={togglePopUp}>閉じる</button>
           </div>
