ac030470f609395cb03b76399d85133d6dc36368
hiromichi-5 <137396801+hiromichi-5@users.noreply.github.com>
2025-01-16 11:36:18 +0900

    desgin

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,12 @@
 import "./header.css";
+import checkmark from "./checkmark.png";
+import green_subject from "./green_subject.png";
+import yellow_subject from "./yellow_subject.png";
+import hover_green from "./hover_green.png";
+import hover_red from "./hover_red.png";
+import download_icon from "./download_icon.png";
+import upload_icon from "./upload_icon.png";
+import caution from "./caution.png";
 
 const Header = () => {
   return (
@@ -13,35 +21,78 @@ const Header = () => {
           成績ファイルは、ローカルで処理され、外部にアップロードされることはありません。
         </div>
         <div className="function">
-          <div>履修中・修得済みの科目は緑色で表示されます</div>
-          <div>
-            未修得の科目は黄色で表示されます
-            <div className="caution">
-              選択科目の場合、最低単位数を満たしていない場合、黄色で表示されます
-            </div>
+          <div className="function-row">
+            <img src={checkmark} alt="checkmark" className="checkmark-icon" />
+            <div>履修中・修得済みの科目は緑色で表示されます</div>
+            <img
+              src={green_subject}
+              alt="green_subject"
+              className="subject-example"
+            />
           </div>
-          <div>
-            履修中・修得済みの科目は緑色で表示されます
-            <div className="caution">
-              必修科目で未履修の場合は科目名が赤色になります
+          <div className="function-row">
+            <img src={checkmark} alt="checkmark" className="checkmark-icon" />
+            <div>
+              未修得の科目は黄色で表示されます
+              <div className="detail">
+                選択科目の場合、最低単位数を満たしていない場合、黄色で表示されます
+              </div>
             </div>
+            <img
+              src={yellow_subject}
+              alt="yellow_subject"
+              className="subject-example"
+            ></img>
           </div>
-          <div>
-            黄色の部分に注意して履修を組むことで、卒業要件を満たすことができます
+          <div className="function-row">
+            <img src={checkmark} alt="checkmark" className="checkmark-icon" />
+            <div>
+              ホバーすることで詳細な履修状況が表示されます
+              <div className="detail">
+                必修科目で未履修の場合は科目名が赤色になります
+              </div>
+            </div>
+            <img
+              src={hover_green}
+              alt="hover_green"
+              className="hover-example"
+            ></img>
+            <img
+              src={hover_red}
+              alt="hover_red"
+              className="hover-example"
+            ></img>
+          </div>
+          <div className="function-row">
+            <img src={checkmark} alt="checkmark" className="checkmark-icon" />
+            <div>
+              黄色の部分に注意して履修を組むことで、卒業要件を満たすことができます
+            </div>
           </div>
         </div>
       </div>
       <div className="usage">
         <h2>使い方</h2>
         <div>
-          <div>
+          <div className="usage-row">
+            <img
+              src={download_icon}
+              alt="download_icon"
+              className="usage-icon"
+            />
             TwinsからCSVファイルをダウンロードする
             <div>CSVのダウンロード方法はこちら</div>
           </div>
-          <div>「ファイルを選択」でCSVファイルをアップロードする</div>
-          <div>
-            Google
-            Chromeのみ対応しています。また、PCから全画面ウインドウの状態でご使用ください。
+          <div className="usage-row">
+            <img src={upload_icon} alt="upload" className="usage-icon" />
+            「ファイルを選択」でCSVファイルをアップロードする
+          </div>
+          <div className="usage-caution">
+            <img src={caution} alt="caution" className="usage-caution-icon" />
+            <span>
+              Google
+              Chromeのみ対応しています。また、PCから全画面ウインドウの状態でご使用ください。
+            </span>
           </div>
         </div>
       </div>
