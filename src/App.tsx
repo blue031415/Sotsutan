1e9f94196cb216543857c83befb402ffab5c3d6d
blue031415 <blue031415@gmail.com>
2025-01-20 18:32:17 +0900

    fix :単位数の 吹き出しの文字とスタイルにの変更

--- src/App.tsx
+++ src/App.tsx
@@ -783,7 +783,7 @@ function App() {
           </button>
         </div>
         <div className="basic-white-area">
-          <p>現在修得済み：{unit_basic}単位</p>
+          <p>修得済・履修中：{unit_basic}単位</p>
         </div>
         <div>
           <ElectivePopup
@@ -846,7 +846,7 @@ function App() {
           </button>
         </div>
         <div className="advanced-white-area">
-          <p>現在修得済み：{unit_advanced}単位</p>
+          <p>修得済・履修中：{unit_advanced}単位</p>
         </div>
         <ElectivePopup
           isOpen={isPopupOpen}
@@ -904,7 +904,7 @@ function App() {
         </div>
         <div className="common-white-area">
           <p>
-            現在修得済み：
+            修得済・履修中：
             {unit_gakusi +
               (unit_electivePE === null ? 0 : unit_electivePE) +
               (unit_electiveLanguage === null ? 0 : unit_electiveLanguage) +
@@ -1219,7 +1219,7 @@ function App() {
         </div>
         <div className="relation-white-area">
           <p>
-            現在修得済み：
+            修得済・履修中：
             {unit_otherSubjects +
               (unit_electiveGBGE === null ? 0 : unit_electiveGBGE) +
               (unit_electiveMuseum === null ? 0 : unit_electiveMuseum)}
@@ -1259,7 +1259,7 @@ function App() {
         <div className="alert_lack_of_elective_units">
           <div className="location_of_balloon">
             <div className="balloon2-left">
-              <p>現在修得済み:{elective_units}単位</p>
+              <p>修得済・履修中:{elective_units}単位</p>
               <p>あと{74 - elective_units}単位必要!</p>
             </div>
           </div>
@@ -1270,7 +1270,7 @@ function App() {
         <div className="fulfill_elective_units">
           <div className="location_of_balloon">
             <div className="balloon2-left">
-              <p>現在修得済み:{elective_units}単位</p>
+              <p>修得済・履修中:{elective_units}単位</p>
               <p>単位充足!</p>
             </div>
           </div>
